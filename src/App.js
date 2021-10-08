97051d15e01479266421fbceea5369a2faaed10f
Tim Marchant <fromtimwithcode@gmail.com>
2021-10-08 16:25:28 -0500

    fetch and display data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getData } from "./actions";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const { data, error, isFetching, getData } = props;
+  console.log(props.data.usd);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (error) {
+    return <h2>We got an error: {error}</h2>;
+  }
+
+  if (isFetching) {
+    return <h2>Fetching data for you!</h2>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        {data.btc.name} / {data.usd.name}
+      </h2>
+      <h3>
+        ({data.btc.unit} / {data.usd.unit})
+      </h3>
+      <h4>${data.usd.value}</h4>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    data: state.data,
+    isFetching: state.isFetching,
+    error: state.error,
+  };
+};
+
+export default connect(mapStateToProps, { getData })(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,47 @@
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAIL } from "../actions";
+
 const initialState = {
-  test: "test",
+  data: {
+    btc: {
+      name: "Bitcoin",
+      unit: "BTC",
+      value: 1.0,
+      type: "crypto",
+    },
+    usd: {
+      name: "US Dollar",
+      type: "fiat",
+      unit: "$",
+      value: 54147.313,
+    },
+  },
+  isFetching: false,
+  error: "",
 };
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_START:
+      return {
+        ...state,
+        data: {},
+        isFetching: true,
+        error: "",
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        data: action.payload,
+        isFetching: false,
+        error: "",
+      };
+    case FETCH_FAIL:
+      return {
+        ...state,
+        data: {},
+        isFetching: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
